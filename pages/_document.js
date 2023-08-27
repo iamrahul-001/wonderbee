3a8b7bfacf4bd8666d5c6c921f21ea45fd7e227b
iamrahul-001 <115921445+iamrahul-001@users.noreply.github.com>
2023-08-27 18:01:55 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+        <Head>
+      <title>Wonder Bee Honey</title>
+      <meta name='description' content='Raw & Natural Honey straight From Bee Hives.' />
+      <meta name='keywords' content='wonder bee, wonder bee honey, wonderbee ,  wonder bee honey products, honey , honey products, wonder honey'/>
+      </Head>
       <body>
         <Main />
         <NextScript />
