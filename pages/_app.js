e706c2ca65806bcdef6477c345fa6e0815a9bf91
iamrahul-001 <115921445+iamrahul-001@users.noreply.github.com>
2023-09-24 19:57:02 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,24 @@ const GlobalStyles = createGlobalStyle`
     margin:0;
     font-family: 'Poppins', sans-serif;
   }
+  .float{
+    position:fixed;
+    width:60px;
+    height:60px;
+    bottom:40px;
+    right:40px;
+    background-color:#25d366;
+    color:#FFF;
+    border-radius:50px;
+    text-align:center;
+    font-size:30px;
+    box-shadow: 2px 2px 3px #999;
+    z-index:100;
+  }
+  
+  .my-float{
+    margin-top:16px;
+  }
 `;
 
 export default function App({ Component, pageProps }) {
