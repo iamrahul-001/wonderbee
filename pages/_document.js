59231ab730209b228ddf6b96d52e3b8d04a56043
iamrahul-001 <115921445+iamrahul-001@users.noreply.github.com>
2023-09-24 19:57:36 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
         <Head>
       <title>Wonder Bee Honey</title>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"/>
       <meta name='description' content='Raw & Natural Honey straight From Bee Hives.' />
       <meta name='keywords' content='wonder bee, wonder bee honey, wonderbee ,  wonder bee honey products, honey , honey products, wonder honey'/>
       </Head>
